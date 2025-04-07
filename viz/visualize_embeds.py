fd4dc2b39b55314237c209122d0097b5dc1a5bf4
dkytezab <danielkytezable@gmail.com>
2025-04-07 16:27:59 -0400

    multple polts generated per diff_step

--- viz/visualize_embeds.py
+++ viz/visualize_embeds.py
@@ -1,24 +1,33 @@
 import torch
+import yaml
 import torchaudio
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-embeddings = torch.load("data/embeddings/CLAP/embeddings.pt")
-embeddings = embeddings.cpu().detach().numpy()
+with open("embeddings/embed_config.yml", "r") as file:
+    embed_config = yaml.safe_load(file)
 
-# Perform PCA to reduce dimensionality
-pca = PCA(n_components=2)
-embeddings_2d = pca.fit_transform(embeddings)
+with open("diffusion/diff_config.yml", "r") as file:
+    diff_config = yaml.safe_load(file)
 
-# Make array of 32 0s and then 32 1s
-labels = np.array([0] * 32 + [1] * 32)
+BATCH_SIZE = diff_config['batch_size']
+MODEL = embed_config['model']
+STEPS = embed_config['steps']
 
-# Plot the 2D embeddings
-plt.figure(figsize=(10, 8))
-plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], alpha=0.5, c=labels, cmap='viridis')
-plt.colorbar()
-plt.title("2D PCA of CLAP Audio Embeddings")
-plt.xlabel("PCA Component 1")
-plt.ylabel("PCA Component 2")
-plt.savefig("viz/outputs/outputs.png")
+for step in STEPS:
+    embeddings = torch.load(f"data/generated/diff_step_{step}/{MODEL}_embeddings.pt")
+    embeddings = embeddings.cpu().detach().numpy()
+
+    pca = PCA(n_components=2)
+    embeddings_2d = pca.fit_transform(embeddings)
+
+    labels = np.array([0] * BATCH_SIZE + [1] * BATCH_SIZE)
+
+    plt.figure(figsize=(10, 8))
+    plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], alpha=0.5, c=labels, cmap='viridis')
+    plt.colorbar()
+    plt.title("2D PCA of CLAP Audio Embeddings")
+    plt.xlabel("PCA Component 1")
+    plt.ylabel("PCA Component 2")
+    plt.savefig(f"viz/outputs/diff_step_{step}_{MODEL}_outputs.png")
