d6ebfd3c43232e97e37613c67de3754a9158e96b
Coby Mulliken <jacob_mulliken@brown.edu>
2025-04-15 23:29:45 -0400

    added musiccaps prompts and scripts to obtain them

---
+++ data/prompts/parse_musiccaps.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pathlib import Path
+
+csv_path = Path.home() / ".cache" / "kagglehub" / "datasets" / "googleai" / "musiccaps" / "versions" / "1" / "musiccaps-public.csv"
+df = pd.read_csv(csv_path)
+subset = df[df["is_balanced_subset"] == True]  # select entries in genre-balanced subset
+prompts = subset["caption"].tolist()
+
+# save the prompts to a text file
+with open("data/prompt.txt", "w") as f:
+    for prompt in prompts:
+        f.write(prompt + "\n")
