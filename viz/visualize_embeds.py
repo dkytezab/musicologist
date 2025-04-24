f29156225806f0a6c45fdbdc5fb5e3f3e5c797c4
dkytezab <danielkytezable@gmail.com>
2025-04-23 21:22:58 -0400

    viz

--- viz/visualize_embeds.py
+++ viz/visualize_embeds.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
+with open("data/prompt.txt", "r") as f:
+    num_prompts = sum(1 for _ in f)
+
 with open("embeddings/embed_config.yml", "r") as file:
     embed_config = yaml.safe_load(file)
 
@@ -12,22 +15,23 @@ with open("diffusion/diff_config.yml", "r") as file:
     diff_config = yaml.safe_load(file)
 
 BATCH_SIZE = diff_config['batch_size']
-MODEL = embed_config['model']
+MODELS = embed_config['model']
 STEPS = embed_config['steps']
 
-for step in STEPS:
-    embeddings = torch.load(f"data/generated/diff_step_{step}/{MODEL}_embeddings.pt")
-    embeddings = embeddings.cpu().detach().numpy()
+for MODEL in MODELS:
+    for step in STEPS:
+        embeddings = torch.load(f"data/generated/diff_step_{step}/{MODEL}_embeddings.pt")
+        embeddings = embeddings.cpu().detach().numpy()
 
-    pca = PCA(n_components=2)
-    embeddings_2d = pca.fit_transform(embeddings)
+        pca = PCA(n_components=2)
+        embeddings_2d = pca.fit_transform(embeddings)
 
-    labels = np.array([0] * BATCH_SIZE + [1] * BATCH_SIZE)
+        labels = np.concatenate([np.full(BATCH_SIZE, i) for i in range(num_prompts)])
 
-    plt.figure(figsize=(10, 8))
-    plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], alpha=0.5, c=labels, cmap='viridis')
-    plt.colorbar()
-    plt.title("2D PCA of CLAP Audio Embeddings")
-    plt.xlabel("PCA Component 1")
-    plt.ylabel("PCA Component 2")
-    plt.savefig(f"viz/outputs/diff_step_{step}_{MODEL}_outputs.png")
+        plt.figure(figsize=(10, 8))
+        plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], alpha=0.5, c=labels, cmap='viridis')
+        plt.colorbar()
+        plt.title("2D PCA of {MODEL} Audio Embeddings")
+        plt.xlabel("PCA Component 1")
+        plt.ylabel("PCA Component 2")
+        plt.savefig(f"viz/outputs/diff_step_{step}_{MODEL}_outputs.png")
