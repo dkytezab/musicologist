139de0dc47feb763ce0e37569adddf873450d5c7
Coby Mulliken <jacob_mulliken@brown.edu>
2025-04-23 22:03:08 -0400

    changed daniel's one line lol

--- diffusion/utils.py
+++ diffusion/utils.py
@@ -47,9 +47,9 @@ def save_audio(
         for j, sample in enumerate(audio):
 
             #filename = f"{output_dir}/prompt_{prompt_index}/sample_{j}_truncation_{truncation_ts[len(truncation_ts) - i - 1]}.wav"
-            # os.makedirs(os.path.dirname(filename), exist_ok=True)
 
             filename = f"{output_dir}/diff_step_{truncation_ts[len(truncation_ts) - i - 1]}/steps_{steps}_prompt_{prompt_index}_sample_{j}.wav"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
             torchaudio.save(filename, sample.cpu(), sample_rate)
 
             if verbose:
